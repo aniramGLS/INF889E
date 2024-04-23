6d001b431f6d5e45c8e03b8d18f4b9916e5de5f5
aniramGLS <167656861+aniramGLS@users.noreply.github.com>
2024-04-23 18:48:54 -0400

    Create SVM.py

---
+++ SVM.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn import svm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.feature_selection import RFE
+from sklearn.metrics import  accuracy_score, confusion_matrix, ConfusionMatrixDisplay, classification_report
+from sklearn.model_selection import cross_val_score
+
+
+
+#################################
+#####    MODEL TRAINING     #####
+#################################
+
+
+
+def run_SVM(X_train, y_train, X_test, y_test):
+    """
+    Training SVM model on training set and evaluation of the model
+
+    Keyword arguments:
+    X_train, y_train -- training dataset
+    X_test, y_test -- testing dataset
+    """
+    model = svm.SVC(C=1.0, kernel='linear', class_weight = None) ### model initialization
+    rfe = RFE(model, n_features_to_select = 200, step = 2)  ### feature selection initialization to keep only 200 features
+    X_train = rfe.fit_transform(X_train, y_train)       #### feature selection on training dataset
+    X_test = rfe.transform(X_test)              #### feature selection on testing dataset
+    model.fit(X_train, y_train)
+    y_prediction = model.predict(X_test)        ### model prediction
+    print(classification_report(y_test, y_prediction, digits = 3))   ### classification evaluation for all superfamily 
+    c_matrix = confusion_matrix(y_test, y_prediction, labels=model.classes_)
+    figure = ConfusionMatrixDisplay(confusion_matrix=c_matrix, display_labels=model.classes_) ### plot confusion matrix graph
+    
+    figure.plot(xticks_rotation = 90)   
+    plt.subplots(figsize=(15, 10))
+    plt.savefig("confusion_matrix_SVM.png")         ### save matrix figure
+
