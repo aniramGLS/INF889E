6895d2699c915d6a56d345d0141983b95a56a9b7
aniramGLS <167656861+aniramGLS@users.noreply.github.com>
2024-04-24 17:55:06 -0400

    Update features.py

--- features.py
+++ features.py
@@ -5,23 +5,33 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
 
-
 #################################
 #####    DATA SCALING       #####
 #################################
 
 def normalization(X):
-    minMaxScaler = MinMaxScaler(feature_range = (0,1))
+    """
+    Normalization of the dataset
+    """
+    standardScaler = StandardScaler()
     
-    return minMaxScaler.fit_transform(X)
+    return standardScaler.fit_transform(X)
 
 
 ##################################
 #####     VISUALIZATION      #####
 ##################################
 
-
 def generateScatterPlot(title, figure_width, figure_height, data, X, y, n_components):
+    """
+    Generate PCA visualization plot
+    
+    Keyword arguments:
+    title -- Title of the graph
+    figure_width, figure_height -- width and height size for the grapg
+    data, X, y -- dataset
+    n_components -- number of the component used for the analysis
+    """
     # If 2d dimensions
     if n_components == 2:
         # Initialize a 2-dimensional figure
@@ -38,19 +48,14 @@ def generateScatterPlot(title, figure_width, figure_height, data, X, y, n_compon
               "tab:green", "tab:red", 
               "tab:purple", "tab:brown", 
               "tab:pink", "tab:grey", 
-              "black", "aquamarine",
-              "orchid", "yellowgreen",
-              "tan", "azure",
-              "gold", "fuschia",
-              "crimson", "darkblue",
-              "navy", "ivory",
-              "tab:olive", "tab:cyan"]
+              "tab:olive", "tab:cyan",]
     
     # Iterate through the targets
     for i, target in enumerate(y):
         # Set the list of axis positions
         x = []
         y = []
+        z = []
         # If the number of targets is less than 10
         if i < 10:
             color = colors[i]
@@ -71,23 +76,30 @@ def generateScatterPlot(title, figure_width, figure_height, data, X, y, n_compon
                 # Save the value of the positions
                 x.append(X[i][0])
                 y.append(X[i][1])
+                if n_components == 3: z.append(X[i][2])
               
         # Add the current scatter plot to the figure
-        ax.scatter(x, y, c = color, label = target, alpha = 0.75, marker=marker)
+        if n_components == 2:
+            ax.scatter(x, y, c = color, label = target, alpha = 0.75, edgecolors = 'none', marker=marker)
+        else:
+            ax.scatter(x, y, z, c = color, label=target,alpha=0.75, edgecolors='none', marker=marker)
 
     # Display the grid
     ax.grid(True)
     # Set the legend parameters
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), prop = {'size': 10})
+    ax.legend(loc = 2, prop = {'size': 10})
     # Set the tite
     plt.title(title)
     # Set axes labels
-    plt.xlabel('PC1')
-    plt.ylabel('PC2')
-    # Display the figure
-    plt.savefig('PCA_transposable_elements.png')
-    plt.show()
-
+    if n_components == 2:
+        plt.xlabel('PC1')
+        plt.ylabel('PC2')
+    else: 
+        ax.set_xlabel('PC1')
+        ax.set_ylabel('PC2')
+        ax.set_zlabel('PC3')
+    # Displqy the figure
+    plt.savefig(f"PCA_transposable_{n_components}.png")
 
 def run_pca(X_train, y_train, train_data):
     """
@@ -101,7 +113,7 @@ def run_pca(X_train, y_train, train_data):
     pca = PCA(n_components = 2)
     X_pca =  pca.fit_transform(X_train)
 
-    generateScatterPlot(title= "Scatter plot of a two-dimensional PCA applied to the training data", 
+    generateScatterPlot(title= "Scatter plot of a tree-dimensional PCA applied to the training data", 
                     figure_width = 15, 
                     figure_height = 12, 
                     data = train_data, 
